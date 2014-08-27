29990166b14e13344c6d3bdaf97183286b1ff99e
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-08-27 06:13:20 +0300

    seq finder

--- main.rb
+++ main.rb
@@ -1,9 +1,22 @@
 require 'oily_png'
 
+def seq_sort(array)
+  finished = [array.first], i = 0
+  array.each_cons(2) do |(a,b)|
+    if b - a == 1
+      finished[i] << b
+    else
+      finished[i += 1] = [b]
+    end
+  end
+  finished
+end
+
 image = ChunkyPNG::Image.from_file('github.png')
 working_image = image.dup
 
 pixels = []
+
 working_image.pixels.each_with_index do |pixel,i|
   r = ChunkyPNG::Color.r(pixel)
   g = ChunkyPNG::Color.g(pixel) 
@@ -13,24 +26,11 @@ working_image.pixels.each_with_index do |pixel,i|
   end
 end
 
-def seq_sort(array)
-  finished = [array.first], i = 0
-
-  array.each_cons(2) do |(a,b)|
-    if b - a == 1
-      finished[i] << b
-    else
-      finished[i += 1] = [b]
-    end
-  end
-
-  finished
-end
-
 seq = seq_sort(pixels)
 c = []
-seq.each do |s|
-  c << s.count
+
+seq.each_with_index do |s,i|
+  c << s.count if s.count > 2
 end
 
 abort c.inspect
