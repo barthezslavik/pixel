c7945ca4412e5a6807dbbb1bc8bb552941bd924b
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-08-30 05:37:05 +0300

    add pattern

--- main.rb
+++ main.rb
@@ -10,11 +10,19 @@ end
 
 image = ChunkyPNG::Image.from_file('github.png')
 working_image = image.dup
+data = []
 
 (0..1365).each do |w|
   (0..767).each do |h|
     pixel = working_image.get_pixel(w,h)
-    puts [w,h].inspect if color?(pixel, 217,217,217)
+    data << [w,h] if color?(pixel, 216,216,216)
   end
 end
 
+pattern = [
+  [0,0,0,1,1],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [1,0,0,0,0],
+  [1,0,0,0,0],
+]
