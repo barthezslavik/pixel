a6aa1b9534f536a75fd618752305eaaeb2e54b14
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-08-30 04:54:09 +0300

    refactoring

--- main.rb
+++ main.rb
@@ -1,20 +1,20 @@
 require 'matrix'
 require 'oily_png'
- 
+
+def color?(pixel, r,g,b)
+  px_r = ChunkyPNG::Color.r(pixel)
+  px_g = ChunkyPNG::Color.g(pixel) 
+  px_b = ChunkyPNG::Color.b(pixel)
+  true if px_r == r && px_g == g && px_b == b
+end
+
 image = ChunkyPNG::Image.from_file('github.png')
 working_image = image.dup
 
 (0..1365).each do |w|
   (0..767).each do |h|
     pixel = working_image.get_pixel(w,h)
-    r = ChunkyPNG::Color.r(pixel)
-    g = ChunkyPNG::Color.g(pixel) 
-    b = ChunkyPNG::Color.b(pixel)
-
-    puts [w,h] if color?(217,217,217)
+    puts [w,h].inspect if color?(pixel, 217,217,217)
   end
 end
 
-def color?(r,g,b)
-  true if r == r && g == g && b == b
-end
