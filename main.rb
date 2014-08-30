b64db07ebde953799b0d68c8a0e9f0099c54e0bb
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-08-30 04:49:29 +0300

    find by color

--- main.rb
+++ main.rb
@@ -4,9 +4,15 @@ require 'oily_png'
 image = ChunkyPNG::Image.from_file('github.png')
 working_image = image.dup
 
-pixel = working_image.get_pixel(1365,767)
-r = ChunkyPNG::Color.r(pixel)
-g = ChunkyPNG::Color.g(pixel) 
-b = ChunkyPNG::Color.b(pixel)
+(0..1365).each do |w|
+  (0..767).each do |h|
+    pixel = working_image.get_pixel(w,h)
+    r = ChunkyPNG::Color.r(pixel)
+    g = ChunkyPNG::Color.g(pixel) 
+    b = ChunkyPNG::Color.b(pixel)
 
-abort [r,g,b].inspect
+    puts [w,h].inspect if r == 217 && g == 217 && b == 217
+  end
+end
+
+
