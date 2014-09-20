6918547e7fdfc01d7fada55cc8c53d90bee7c3b2
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-09-20 12:42:32 +0300

    move samples

--- area.rb
+++ area.rb
@@ -7,10 +7,28 @@ sample = [
   [0,1,1,0,0,0],
 ]
 
-pixel_range = PixelRange.new
-data = pixel_range.fetch(sample)
+sample2 = [
+  [0,0,0,0,0,0,0],
+  [0,0,0,1,1,1,0],
+  [0,0,1,0,0,0,1],
+  [0,0,1,0,0,0,1],
+  [0,0,1,0,0,0,1],
+  [0,0,1,0,0,0,1],
+  [0,0,1,0,0,0,1],
+  [0,0,0,1,1,1,0],
+]
 
-sample.each do |s|
+sample3 = [
+  [0,0,1,1,1,1,1,1,1,0,0],
+  [0,1,0,0,0,0,0,0,0,1,0],
+  [0,1,0,0,0,0,0,0,0,1,0],
+  [0,0,1,1,1,1,1,1,1,0,0],
+]
+
+pixel_range = PixelRange.new
+data = pixel_range.fetch(sample2)
+
+sample2.each do |s|
   puts s.inspect
 end
 
