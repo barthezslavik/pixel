b4217a1fe263308867276c393b250d7330ef02e9
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-09-20 12:06:13 +0300

    refactoring

--- pixel_range.rb
+++ pixel_range.rb
@@ -16,29 +16,25 @@ sample3 = [
   [0,0,1,1,1,1,1,1,1,0,0],
 ]
 
-
 class PixelRange
-  def find_left(row, x)
-    (1..row.count).each do |i|
-      return x if row[x-i] == 1
+  def find_border(struct, pointer, direction)
+    if direction == :head
+      search = struct[0..pointer]
+      index = search.each_with_index.select { |num, index| num == 1 }.map { |pair| pair[1] }.max
+      if index
+        return index+1
+      else
+        return pointer
+      end
     end
-  end
-
-  def find_right(row, x)
-    (1..row.count).each do |i|
-      return x if row[x+i] == 1
-    end
-  end
-
-  def find_up(col, y)
-    (1..col.count).each do |i|
-      return y if col[y-i] == 1
-    end
-  end
-
-  def find_down(col, y)
-    (1..col.count).each do |i|
-      return y if col[y+i] == 1
+    if direction == :tail
+      search = struct[pointer..struct.count]
+      index = search.each_with_index.select { |num, index| num == 1 }.map { |pair| pair[1] }.min
+      if index
+        return index+pointer-1
+      else
+        return pointer+search.count-1
+      end
     end
   end
 
@@ -47,13 +43,11 @@ class PixelRange
     sample.each_with_index do |row, y|
       row.each_with_index do |r, x|
         next if r == 1
-        min_x = find_left(row, x)
-        max_x = find_right(row, x)
-        min_y = find_up(sample.transpose[y], y)
-        max_y = find_down(sample.transpose[y], y)
-        max_x = max_x.max-1 if max_x.is_a? Range
-        max_y = max_y.max-1 if max_y.is_a? Range 
-        data << [min_x, max_x, min_y, max_y]
+        min_x = find_border(row, x, :head)
+        max_x = find_border(row, x, :tail)
+        min_y = find_border(sample.transpose[y], y, :head)
+        max_y = find_border(sample.transpose[y], y, :tail)
+        data << [x, y, min_x, max_x, min_y, max_y]
         #data << { x:x, y:y, min_x: min_x, max_x: max_x, min_y: min_y, max_y: max_y }
       end
     end
