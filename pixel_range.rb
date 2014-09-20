60e142cb1b73464c969c2367eb32e46345ab9bf4
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-09-20 12:41:27 +0300

    fix y

--- pixel_range.rb
+++ pixel_range.rb
@@ -19,21 +19,26 @@ sample3 = [
 class PixelRange
   def find_border(struct, pointer, direction)
     if direction == :head
+
       search = struct[0..pointer]
       index = search.each_with_index.select { |num, index| num == 1 }.map { |pair| pair[1] }.max
+      
       if index
         return index+1
       else
-        return pointer
+        return 0
       end
     end
+
     if direction == :tail
+
       search = struct[pointer..struct.count]
       index = search.each_with_index.select { |num, index| num == 1 }.map { |pair| pair[1] }.min
+      
       if index
         return index+pointer-1
       else
-        return pointer+search.count-1
+        return struct.count-1
       end
     end
   end
@@ -43,14 +48,17 @@ class PixelRange
     sample.each_with_index do |row, y|
       row.each_with_index do |r, x|
         next if r == 1
+
         min_x = find_border(row, x, :head)
         max_x = find_border(row, x, :tail)
-        min_y = find_border(sample.transpose[y], y, :head)
-        max_y = find_border(sample.transpose[y], y, :tail)
+        min_y = find_border(sample.transpose[x], y, :head)
+        max_y = find_border(sample.transpose[x], y, :tail)
+        
         data << [x, y, min_x, max_x, min_y, max_y]
         #data << { x:x, y:y, min_x: min_x, max_x: max_x, min_y: min_y, max_y: max_y }
       end
     end
+
     return data
   end
 end
