31ac092b130e91f878b098eeeb001a909c1fcb68
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-10-11 07:47:09 +0300

    dry

--- grid.rb
+++ grid.rb
@@ -2,7 +2,7 @@ require 'oily_png'
 require 'pp'
 
 def color(pixel)
-  [ChunkyPNG::Color.r(pixel), ChunkyPNG::Color.g(pixel), ChunkyPNG::Color.b(pixel)]
+  [:r,:g,:b].map{|c| ChunkyPNG::Color.send(c, pixel)}
 end
 
 image = ChunkyPNG::Image.from_file('mini.png')
@@ -15,5 +15,7 @@ data = []
     data << { x: x, y:y, color: color(working_image.get_pixel(x,y)) }
   end
 end
+
+
 
 pp data
