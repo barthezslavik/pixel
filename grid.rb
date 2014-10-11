4cb9c5f12c51ce3df62f82e1caa48f43c0fc1c26
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-10-11 08:00:57 +0300

    grid original

--- grid.rb
+++ grid.rb
@@ -5,17 +5,23 @@ def color(pixel)
   [:r,:g,:b].map{|c| ChunkyPNG::Color.send(c, pixel)}
 end
 
-image = ChunkyPNG::Image.from_file('mini.png')
+image = ChunkyPNG::Image.from_file('grid_original.png')
 working_image = image.dup
 
+#size = [80, 43]
+
 data = []
+by_x = []
 
 (0..image.width-1).each do |x|
+  data[x] = 0
   (0..image.height-1).each do |y|
-    data << { x: x, y:y, color: color(working_image.get_pixel(x,y)) }
+    if color(working_image.get_pixel(x,y)) == [255,255,255]
+      data[x] += 1
+    end
+    #data << { x: x, y:y, color: color(working_image.get_pixel(x,y)) }
   end
+  by_x << data[x]
 end
 
-
-
-pp data
+abort by_x.inspect
