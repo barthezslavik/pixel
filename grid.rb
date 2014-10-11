ebe1de3fa3c648e7b12e40d4a5ecfc5da3524e52
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-10-11 08:32:32 +0300

    vertical grids

--- grid.rb
+++ grid.rb
@@ -8,10 +8,10 @@ end
 image = ChunkyPNG::Image.from_file('grid_original.png')
 working_image = image.dup
 
-#size = [80, 43]
-
 data = []
 by_x = []
+
+size = { x:image.width, y:image.height }
 
 (0..image.width-1).each do |x|
   data[x] = 0
@@ -19,9 +19,25 @@ by_x = []
     if color(working_image.get_pixel(x,y)) == [255,255,255]
       data[x] += 1
     end
-    #data << { x: x, y:y, color: color(working_image.get_pixel(x,y)) }
   end
   by_x << data[x]
 end
 
-abort by_x.inspect
+distr = by_x.map {|g| (g*100/size[:y]).to_i }
+
+lines_at = []
+
+distr.each_with_index do |d, i|
+  if d < 95
+    lines_at << i
+  end
+end
+
+png = ChunkyPNG::Image.new(size[:x], size[:y], :white)
+lines_at.each do |x|
+  (0..size[:y]-1).each do |y|
+    png[x,y] = ChunkyPNG::Color(:black)
+  end
+end
+
+png.save('out.png')
