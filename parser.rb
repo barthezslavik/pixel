992e17938bcf0bbccd759787cc9c6818f6827832
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-10-26 08:28:41 +0200

    hold

--- parser.rb
+++ parser.rb
@@ -31,7 +31,7 @@ class Parser
       end
     end
 
-    ranges.each do |r|
+    ranges.each_with_index do |r,i|
       fill = []
       (0..image.height-1).each do |y|
         clear = true
@@ -41,10 +41,24 @@ class Parser
         end
         fill << [clear,y]
       end
+
       top = fill.each { |f| break f.last if f.first == false }
       bottom = fill.reverse.each { |f| break f.last if f.first == false }
-      objects << { min_x: r.first, min_y: top, max_x: r.last, max_y: bottom }
+      objects << { element_id: i+1, min_x: r.first, min_y: top, max_x: r.last, max_y: bottom }
     end
+
+    objects[0] = { html: { tag: "img", src: "image1.png" } }.merge!(objects[0])
+    objects[1] = { html: { tag: "input", type: "text", placeholder: "Search GitHub" } }.merge!(objects[1])
+    objects[2] = { html: { tag: "li" }, text: "Explore" }.merge!(objects[2])
+    objects[3] = { html: { tag: "li" }, text: "Gist" }.merge!(objects[3])
+    objects[4] = { html: { tag: "li" }, text: "Blog" }.merge!(objects[4])
+    objects[5] = { html: { tag: "li" }, text: "Help" }.merge!(objects[5])
+
+    objects[6] = { html: { tag: "img" }, src: "image2.png" }.merge!(objects[6])
+    objects[7] = { html: { tag: "li" }, text: "barthezslavik" }.merge!(objects[7])
+    objects[8] = { html: { tag: "img" }, src: "image3.png" }.merge!(objects[8])
+    objects[9] = { html: { tag: "img" }, src: "image4.png" }.merge!(objects[9])
+
   end
 
   def get_distances
@@ -61,19 +75,19 @@ class Parser
     peak_index =  distances.each_with_index.max[1]
     #objects[0..peak_index-1].each { |o| o[:deep] = 1 }
     #objects[peak_index..-1].each { |o| o[:deep] = 1 }
-    
+
     min_x_left = objects[0..peak_index].map {|o| o[:min_x]}.min
     min_y_left = objects[0..peak_index].map {|o| o[:min_y]}.min
     max_x_left = objects[0..peak_index].map {|o| o[:max_x]}.max
     max_y_left = objects[0..peak_index].map {|o| o[:max_y]}.max
-    
+
     min_x_right = objects[peak_index+1..-1].map {|o| o[:min_x]}.min
     min_y_right = objects[peak_index+1..-1].map {|o| o[:min_y]}.min
     max_x_right = objects[peak_index+1..-1].map {|o| o[:max_x]}.max
     max_y_right = objects[peak_index+1..-1].map {|o| o[:max_y]}.max
-    
-    objects << { min_x: min_x_left, min_y: min_y_left, max_x: max_x_left, max_y: max_y_left }
-    objects << { min_x: min_x_right, min_y: min_y_right, max_x: max_x_right, max_y: max_y_right }
+
+    #objects << { min_x: min_x_left, min_y: min_y_left, max_x: max_x_left, max_y: max_y_left }
+    #objects << { min_x: min_x_right, min_y: min_y_right, max_x: max_x_right, max_y: max_y_right }
   end
 
   def group_near
@@ -95,8 +109,14 @@ class Parser
     max_x = objects.map { |o| o[:max_x] }.max
     min_y = objects.map { |o| o[:min_y] }.min
     max_y = objects.map { |o| o[:max_y] }.max
-    objects << { min_x: min_x, min_y: min_y, max_x: max_x, max_y: max_y }
-    objects << { min_x: 0, min_y: 0, max_x: @image.width-1, max_y: @image.height-1 }
+    objects << { html: { tag: "div", id: "container"}, css: ["width:#{max_x-min_x}px"], min_x: min_x, min_y: min_y, max_x: max_x, max_y: max_y }
+    objects << { html: { tag: "div", id: "header"}, css: ["width:#{@image.width-1}px", "background:#{rgb_to_hex(background)}"], min_x: 0, min_y: 0, max_x: @image.width-1, max_y: @image.height-1 } 
+  end
+
+  def rgb_to_hex(value)
+    color = ""
+    value.each {|component| color << component.to_s(16) }
+    "##{color}"
   end
 
   def initialize(image)
@@ -113,7 +133,7 @@ class Parser
     get_objects
     get_distances
     group_near
-    left_right
+    left_right rescue nil
     get_container
 
     output = ChunkyPNG::Image.new(@image.width, @image.height, :white)
