be914d22e5cfff32ea27745e4d7e2a86019ef8b1
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-10-26 05:05:39 +0200

    groups

--- parser.rb
+++ parser.rb
@@ -1,5 +1,6 @@
 class Parser
-  attr_accessor :result, :by_x, :by_y, :h_sizes, :data, :objects, :image, :edges
+  attr_accessor :result, :by_x, :by_y, :h_sizes,
+    :data, :objects, :image, :edges, :groups
 
   def color(pixel)
     [:r, :g, :b].map{ |c| ChunkyPNG::Color.send(c, pixel) }
@@ -46,7 +47,46 @@ class Parser
     end
   end
 
+  def get_groups
+    distances = []
+    objects.each_with_index do |o,i|
+      distances << objects[i+1][:min_x] - objects[i][:max_x] rescue next
+    end
+    peak =  distances.each_with_index.max[1]
+    groups << distances[0..peak-1]
+    groups << distances[peak+1..-1]
+  end
+
+  def group_near
+    distances = []
+    objects.each_with_index do |o,i|
+      if i <= objects.count-2
+        d = objects[i+1][:min_x] - objects[i][:max_x]
+        distances << d if d > 0
+      end
+    end
+    glue = []
+    distances.each_with_index do |d,i|
+      glue << [i,i+1] if d < 7
+    end
+
+    glue.each do |g|
+      max_y = [objects[g.first][:max_y], objects[g.last][:max_y]].max 
+      objects << { min_x: objects[g.first][:min_x], min_y: objects[g.first][:min_y], max_x: objects[g.last][:max_x], max_y: max_y}
+    end
+  end
+
+  def get_container
+    min_x = objects.map { |o| o[:min_x] }.min
+    max_x = objects.map { |o| o[:max_x] }.max
+    min_y = objects.map { |o| o[:min_y] }.min
+    max_y = objects.map { |o| o[:max_y] }.max
+    objects << { min_x: min_x, min_y: min_y, max_x: max_x, max_y: max_y }
+    objects << { min_x: 0, min_y: 0, max_x: @image.width-1, max_y: @image.height-1 }
+  end
+
   def initialize(image)
+    @groups = []
     @result = []
     @by_x = []
     @data = []
@@ -54,6 +94,10 @@ class Parser
     @image = ChunkyPNG::Image.from_file(image)
 
     get_objects
+    get_groups
+    get_container
+    group_near
+
     output = ChunkyPNG::Image.new(@image.width, @image.height, :white)
 
     objects.each do |o|
